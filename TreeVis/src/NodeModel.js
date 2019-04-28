93787ffe36fb3224362cded19c980a49607c7a6e
Tai <justataya@gmail.com>
2019-04-28 18:11:49 +0300

    Fixed model classes

--- TreeVis/src/NodeModel.js
+++ TreeVis/src/NodeModel.js
@@ -1,5 +1,6 @@
 class NodeModel extends AbstractModel{
     constructor(node) {
+        super();
         this.node = node;
     }
 }
--- TreeVis/src/TreeModel.js
+++ TreeVis/src/TreeModel.js
@@ -1,5 +1,6 @@
 class TreeModel extends AbstractModel {
     constructor(tree) {
+        super();
         this.tree = tree;
     }
 }
